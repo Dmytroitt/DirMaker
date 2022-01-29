05d0100351be1e40d0b60dd7ef53eaec6292c433
Hojako <89229676+Hojako@users.noreply.github.com>
2022-01-29 21:32:50 +0000

    Update Mkdir.py

--- Mkdir.py
+++ Mkdir.py
@@ -1,6 +1,7 @@
 import os
-nome = str(input('Insira o nome para os diretórios: '))
-quantidade = int(input('Insira a quantidade de diretórios que deseja criar: '))
-for i in range(0, quantidade):
-    os.mkdir(r'{}{:03d}'.format(nome,i))
-print('Diretórios criados!')
+names = str(input('Name for dirs: '))
+nums = int(input('Number of Dirs you want: '))
+
+for i in range(0, nums):
+    os.mkdir(r'{}{:03d}'.format(names,i))
+print('Done, Check your Dirs!')
